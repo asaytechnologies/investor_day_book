84b244c498d952e600ccc9073d73e04aad7b48e6
kortirso <kortirso@gmail.com>
2020-11-28 17:49:33 +0300

    render portfolio selector

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class PortfolioController < ApplicationController
+  before_action :portfolios
   before_action :positions
 
   def index; end
 
   private
+
+  def portfolios
+    @portfolios = current_user.portfolios
+  end
 
   def positions
     @positions = Positions::Fetching::ForPortfolioService.call(user: current_user).result
