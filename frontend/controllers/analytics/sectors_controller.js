664078c0a67804a4ec70bb927732df04238102f9
kortirso <kortirso@gmail.com>
2020-12-08 23:16:22 +0300

    render custom legend for pie chart

--- frontend/controllers/analytics/sectors_controller.js
+++ frontend/controllers/analytics/sectors_controller.js
@@ -13,14 +13,38 @@ export default class extends ApplicationController {
       labels: Object.keys(this.pieValue)
     }
 
-    new Chart(document.getElementById('sectors-pie'), {
+    const pieChart = new Chart(document.getElementById("sectors-pie"), {
       type: 'pie',
       data: data,
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legendCallback: function (chart) {
+          const text = []
+          text.push("<ul class='legend-list'>")
+
+          const data = chart.data
+          const datasets = data.datasets
+          const labels = data.labels
+
+          if (datasets.length) {
+            for (var i = 0; i < datasets[0].data.length; ++i) {
+              text.push('<li><span style="background-color:' + datasets[0].backgroundColor[i] + '"></span>')
+              if (labels[i]) {
+                text.push(labels[i] + ' (' + datasets[0].data[i] + '%)')
+              }
+              text.push('</li>')
+            }
+          }
+          text.push('</ul>')
+          return text.join('')
+        },
         legend: {
-          position: 'right'
+          display: false
         }
       }
     })
+
+    document.getElementById("legend").innerHTML = pieChart.generateLegend()
   }
 }
