4e68a17fbb3587a9a84fe1b2234ec45d6de091aa
kortirso <kortirso@gmail.com>
2020-12-13 23:25:50 +0300

    fix for yahoo finance api

--- app/lib/yahoo_finance_api/summary/asset_profile.rb
+++ app/lib/yahoo_finance_api/summary/asset_profile.rb
@@ -4,6 +4,7 @@ module YahooFinanceApi
   module Summary
     module AssetProfile
       def asset_profile(ticker:)
+        ticker = ticker.gsub('@', '.') if ticker.include?('@')
         response = connection.get("quoteSummary/#{ticker}?modules=assetProfile")
         response.body
       end
