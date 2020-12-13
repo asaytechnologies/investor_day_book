03635050b433c9e2072f5ee0ecc71e0e730ba5f4
kortirso <kortirso@gmail.com>
2020-12-13 15:18:53 +0300

    modify rendering portfolios

--- app/components/portfolios/index/list_element_component.rb
+++ app/components/portfolios/index/list_element_component.rb
@@ -5,7 +5,8 @@ module Portfolios
     class ListElementComponent < ViewComponent::Base
       def initialize(portfolio:)
         @portfolio = portfolio
-        @caches    = @portfolio.cashes.where.not(amount_cents: 0)
+        @income    = @portfolio.cashes.income.where.not(amount_cents: 0)
+        @balance   = @portfolio.cashes.balance.where.not(amount_cents: 0)
       end
     end
   end
