228abb77c1dd1710fc8a633b82ed67a19c8d5aec
Anton Bogdanov <kortirso@gmail.com>
2021-01-10 10:53:35 +0300

    modify select boxes

--- frontend/controllers/shared/navigation_controller.js
+++ frontend/controllers/shared/navigation_controller.js
@@ -1,9 +1,21 @@
 import ApplicationController from "../application_controller"
 
+import { useClickOutside } from "stimulus-use"
+
 export default class extends ApplicationController {
   static targets = ["list"]
 
+  connect () {
+    useClickOutside(this)
+  }
+
   toggle () {
     this.listTarget.classList.toggle("open")
+  }
+
+  close (event) {
+    event.preventDefault()
+
+    this.listTarget.classList.remove("open")
   }
 }
--- frontend/controllers/shared/select_controller.js
+++ frontend/controllers/shared/select_controller.js
@@ -1,15 +1,25 @@
 import ApplicationController from "../application_controller"
 
+import { useClickOutside } from "stimulus-use"
+
 export default class extends ApplicationController {
   static targets = ["list", "value", "valueIndex"]
 
   connect () {
     this.valueTarget.innerHTML = this.data.get("value")
     this.valueIndexTarget.value = this.data.get("valueIndex")
+
+    useClickOutside(this)
   }
 
-  open () {
-    this.listTarget.classList.add("open")
+  toggle () {
+    this.listTarget.classList.toggle("open")
+  }
+
+  close (event) {
+    event.preventDefault()
+
+    this.listTarget.classList.remove("open")
   }
 
   select (event) {
