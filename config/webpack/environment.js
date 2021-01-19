fa0b10035c8a346640d5ab8e0516955deb1f1563
Anton Bogdanov <kortirso@gmail.com>
2021-01-19 08:35:18 +0300

    added js compression

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+environment.plugins.prepend('env',
+  new CompressionPlugin({
+    filename: '[path].gz[query]',
+    algorithm: 'brotliCompress',
+    test: /\.js$|\.css$/,
+    threshold: 10240,
+    minRatio: 0.8,
+    cache: true
+  })
+)
 
 module.exports = environment
