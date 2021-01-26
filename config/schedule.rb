511cbdfefbf1435e6f9c4213d1de5e5421e63047
Bogdanov Anton <kortirso@gmail.com>
2021-01-26 08:01:45 +0300

    change time for schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,16 +5,16 @@ every 1.minute do
 end
 
 # synchronize quotes with MOEX Exchange
-every :day, at: '6:00' do
+every :day, at: '3:00' do
   runner 'Quotes::Synchronization::MoexJob.perform_later'
 end
 
 # synchronize quotes with Tinkoff broker
-every :day, at: ['8:00', '20:00'] do
+every :day, at: ['5:00', '17:00'] do
   runner 'Quotes::Synchronization::TinkoffJob.perform_later'
 end
 
 # synchronize exchange rates
-every :day, at: ['10:00', '14:00', '18:00'] do
+every :day, at: ['7:00', '11:00', '15:00'] do
   runner 'Quotes::Synchronization::ExchangeRatesJob.perform_later'
 end
